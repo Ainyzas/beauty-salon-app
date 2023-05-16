28dc6bea94f372ef69ba14ba9c459d1a8b827ad5
ainas <puidokasainas@gmail.com>
2023-05-16 12:37:30 +0300

    fixed url

--- frontend/src/api-calls/users/index.ts
+++ frontend/src/api-calls/users/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:4000/';
+const API_URI = 'https://beauty-salon-app.up.railway.app/';
 
 export async function createUser(name: string, surname: string, email: string, registrationDate: string) {
   try {
